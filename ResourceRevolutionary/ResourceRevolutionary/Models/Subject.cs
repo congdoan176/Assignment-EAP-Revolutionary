5a2457b81302f33507d625fc6b034074b414a277
unknown <doanncd00530@fpt.edu.vn>
2018-12-26 18:41:11 +0700

    Tao model cho mon hoc.

---
+++ ResourceRevolutionary/ResourceRevolutionary/Models/Subject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResourceRevolutionary.Models
+{
+    public class Subject
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+        public SubjectStatus Status { get; set; }
+
+        public Subject()
+        {
+            this.CreateAt = DateTime.Now;
+            this.UpdateAt = DateTime.Now;
+            this.Status = SubjectStatus.Active;
+        }
+    }
+    public enum SubjectStatus
+    {
+        Active = 1,
+        Deactive = 0
+    }
+}
